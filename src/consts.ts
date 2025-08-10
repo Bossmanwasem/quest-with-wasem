5d324e927d0f911c95ed593a71657893cf006322
Bossmanwasem <jordanwasem93@gmail.com>
2025-08-09 21:22:10 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Quest With Wasem";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Adventure Awaits! DM Wasem's TTRPG hosting Hub. Find your Documents, World lore and VTT information.";
